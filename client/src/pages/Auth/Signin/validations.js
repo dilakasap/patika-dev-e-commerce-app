1e9a646de8724e5eed72ca06b2df6fbcb18292e7
dilakasap <dilakasap@gmail.com>
2021-11-19 00:46:03 +0300

    login page

---
+++ client/src/pages/Auth/Signin/validations.js
@@ -0,0 +1,14 @@
+import * as yup from "yup";
+
+const validations = yup.object().shape({
+  email: yup
+    .string()
+    .email("Geçerli bir email girin")
+    .required("Zorunlu alan "),
+  password: yup
+    .string()
+    .min(8, "Parolanız en az 8 karakter olmalıdır.")
+    .required(),
+});
+
+export default validations;
