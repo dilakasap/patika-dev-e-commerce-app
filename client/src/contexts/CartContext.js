0f24601342b9f05295892daa19604b33626adab4
dilakasap <dilakasap@gmail.com>
2021-11-20 17:02:16 +0300

    product details page add to cart button updated.

--- client/src/contexts/CartContext.js
+++ client/src/contexts/CartContext.js
@@ -4,13 +4,17 @@ const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [items, setItems] = useState([]);
 
+  const addToCart = (data) => {
+    setItems((prev) => [...prev, data]);
+  };
+
   const values = {
     items,
     setItems,
+    addToCart,
   };
 
   return <CartContext.Provider value={values}>{children}</CartContext.Provider>;
 };
 
 export const useCart = () => useContext(CartContext);
-
--- client/src/pages/ProductDetails/index.js
+++ client/src/pages/ProductDetails/index.js
@@ -1,36 +1,42 @@
-import React from 'react'
-import {useParams} from "react-router-dom";
-import {useQuery} from "react-query";
-import { getProductDetails } from '../../helpers/Api';
-import {Box,Text,Button} from "@chakra-ui/react";
-import moment from 'moment';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { getProductDetails } from "../../helpers/Api";
+import { Box, Text, Button } from "@chakra-ui/react";
+import moment from "moment";
 import ImageGallery from "react-image-gallery";
-
+import { useCart } from "../../contexts/CartContext";
 
 function ProductDetails() {
-  const {product_id}=useParams();
-  const {isLoading,isError,data} =useQuery(["product",product_id],()=>getProductDetails(product_id))
-  if (isLoading){
+  const { product_id } = useParams();
+  const { addToCart } = useCart();
+  const { isLoading, isError, data } = useQuery(["product", product_id], () =>
+    getProductDetails(product_id)
+  );
+  if (isLoading) {
     return "Loading...";
   }
-  if(isError){
-    return "Error"+isError.message;
+  if (isError) {
+    return "Error" + isError.message;
   }
   console.log(data);
 
-  const images=data.photos.map((url)=>({original:url}));
+  const images = data.photos.map((url) => ({ original: url }));
   return (
     <div>
-    
-    <Button colorScheme="telegram" >Add to Cart</Button>
-    <Text as="h2" fontSize="2xl">{data.title}</Text>
-    <Text>{moment(data.createdAt).format("DD/MM/YYYY")}</Text>
-     <p>{data.description}</p>
-     <Box margin="10">
-      <ImageGallery items={images}/>
-    </Box>
+      <Button colorScheme="telegram" onClick={() => addToCart(data)}>
+        Add to Cart
+      </Button>
+      <Text as="h2" fontSize="2xl">
+        {data.title}
+      </Text>
+      <Text>{moment(data.createdAt).format("DD/MM/YYYY")}</Text>
+      <p>{data.description}</p>
+      <Box margin="10">
+        <ImageGallery items={images} />
+      </Box>
     </div>
-  )
+  );
 }
 
 export default ProductDetails;
