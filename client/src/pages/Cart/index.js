4671ff46ed6404127bb809ad7772169d56990b66
dilakasap <dilakasap@gmail.com>
2021-11-22 23:31:19 +0300

    products added to cart

--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,6 +1,8 @@
 import { Alert } from '@chakra-ui/alert';
+import { Link } from "react-router-dom";
 import React from 'react'
 import { useCart } from '../../contexts/CartContext';
+import { Image} from '@chakra-ui/image';
 
 function Cart() {
   const {items} = useCart();
@@ -8,6 +10,21 @@ function Cart() {
   return (
     <div>
        {items.length<1 && <Alert status="warning">Sepetinizde ürün bulunmamaktadır!</Alert>}
+
+       {items.length > 0 && <>
+       <ul>
+         {
+           items.map((item)=>(
+            <li key={item._id}>
+              <Link to={`/product/${item._id}`}>
+                {item.title} - {item.price} TL
+                <Image htmlWidth={200} src={item.photos[0]} alt={item.title}/>
+              </Link>
+            </li>
+           ))
+         }
+       </ul>
+       </>}
     </div>
   )
 }
