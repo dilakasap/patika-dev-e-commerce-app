bd0c46de8064932b4a1b0a9db260dc5a60e45a97
dilakasap <dilakasap@gmail.com>
2021-12-09 23:13:03 +0300

    order modal added

--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import { useRef } from "react";
 import { useCart } from "../../contexts/CartContext";
-import { Button, Alert, Image, Box, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Alert,
+  Image,
+  Box,
+  Text,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 
 function Cart() {
   const { items, removeFromCart } = useCart();
+  const initialRef = useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const total = items.reduce((acc, obj) => acc + obj.price, 0);
 
   return (
@@ -15,7 +34,7 @@ function Cart() {
 
       {items.length > 0 && (
         <>
-          <ul style={{listStyleType: "decimal"}}>
+          <ul style={{ listStyleType: "decimal" }}>
             {items.map((item) => (
               <li key={item._id} style={{ marginBottom: 15 }}>
                 <Link to={`/product/${item._id}`}>
@@ -38,9 +57,36 @@ function Cart() {
             ))}
           </ul>
           <Box mt="10">
-            <Button colorScheme="green" mt="2" size="sm">Order</Button>
             <Text fontSize="22">Total:{total} TL</Text>
           </Box>
+          <Button onClick={onOpen} colorScheme="green" mt="2" size="sm">
+            Order
+          </Button>
+
+          <Modal
+            initialFocusRef={initialRef}
+            isOpen={isOpen}
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Create your account</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody pb={6}>
+                <FormControl>
+                  <FormLabel>First name</FormLabel>
+                  <Input ref={initialRef} placeholder="First name" />
+                </FormControl>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3}>
+                  Save
+                </Button>
+                <Button onClick={onClose}>Cancel</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </>
       )}
     </Box>
