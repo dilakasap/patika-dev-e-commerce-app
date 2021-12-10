cfa3c70b72ebaa021f84bf1500428124893b365d
dilakasap <dilakasap@gmail.com>
2021-12-10 12:37:00 +0300

    admin page routers added

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ import ProtectedRoute from "./pages/ProtectedRoute";
 import Cart from "./pages/Cart";
 import Error404 from "./pages/Error404";
 import Admin from "./pages/Admin";
+import Home from "./pages/Admin/Home";
+import Orders from "./pages/Admin/Orders";
+import AdminProducts from "./pages/Admin/AdminProducts";
 
 function App() {
   return (
@@ -23,8 +26,12 @@ function App() {
             <Route path="/cart" element={<Cart />} />
             <Route path="/product/:product_id" element={<ProductDetails />} />
             <Route path="/profile" element={<ProtectedRoute />} />
-            <Route path="*" element={<Error404/>}/>
-            <Route path="/admin" element={<Admin/>}></Route>
+            <Route path="*" element={<Error404 />} />
+            <Route path="/admin" element={<Admin />}>
+              <Route path="/admin" element={<Home />} />
+              <Route path="/admin/orders" element={<Orders />} />
+              <Route path="/admin/products" element={<AdminProducts />} />
+            </Route>
           </Routes>
         </div>
       </div>
